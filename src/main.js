5839b280f959de356827d981d915c1c3abfc184b
i-spin <me@i-sp.in>
2022-07-17 13:37:33 -0700

    set host by env variable

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ fastify.get('/', require('./routes/Home'))
 fastify.get('/plugins', require('./routes/Plugins'))
 fastify.get('/download/:name', require('./routes/Download'))
 
-fastify.listen({ port: process.env.PORT ?? 3000 }, (err) => {
+fastify.listen({ port: process.env.PORT ?? 3000, host: process.env.HOST ?? '127.0.0.1' }, (err) => {
   if (err) {
     console.error(err)
     process.exit(1)
