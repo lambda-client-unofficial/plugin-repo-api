fe92153319fb4ef1a73284a6de63769dc93527ab
i-spin <me@i-sp.in>
2022-07-16 19:52:00 -0700

    add routes to main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { Application } from "https://deno.land/x/abc@v1.3.3/mod.ts";
+import { Application } from 'https://deno.land/x/abc@v1.3.3/mod.ts';
 
 import Home from './routes/Home.ts';
+import Plugins from './routes/Plugins.ts';
+import Download from './routes/Download.ts';
 
 const app = new Application();
 
 app
   .get('/', Home)
+  .get('/plugins', Plugins)
+  .get('/download/:name', Download)
   .start({port: 8080})
+
+console.log('Listening on port 8080.');
