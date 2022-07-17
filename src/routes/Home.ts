004c91c627bd0a2fe8675635ffc0ffe1015b41ca
i-spin <me@i-sp.in>
2022-07-16 19:52:37 -0700

    update main page

--- src/routes/Home.ts
+++ src/routes/Home.ts
@@ -1,5 +1,9 @@
 import { Context } from "https://deno.land/x/abc@v1.3.3/mod.ts";
 
 export default function Home(_ctx: Context) {
-  return "Hello";
+  return `
+    GET /                      Shows this page.
+    GET /plugins               Lists all plugins avalible.
+    GET /download/:name        Returns the link to the plugin download.
+  `;
 }
